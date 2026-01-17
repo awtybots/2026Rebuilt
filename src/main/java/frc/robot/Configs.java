b33345498ccaf0b6ac668c9235a354ee56bfc593
Yonube <blazer2008.mail@gmail.com>
2026-01-17 11:58:26 -0600

    Intake Subsystem

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -1,47 +1,37 @@
 package frc.robot;
 
 import com.revrobotics.spark.config.SparkFlexConfig;
-import com.revrobotics.spark.config.SparkMaxConfig;
-
 import edu.wpi.first.wpilibj2.command.Command;
 
 import com.revrobotics.spark.config.SparkBaseConfig.*;
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.FeedbackSensor;
 
 public final class Configs {
 
         public static final class IntakeSubsystem {
-                public static final SparkFlexConfig l_elevatorMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig r_elevatorMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig r_armMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig l_armMotorConfig = new SparkFlexConfig();
-                // public static final SparkFlexConfig r_armMotorSlowConfig = new SparkFlexConfig();
-                // public static final SparkFlexConfig l_armMotorSlowConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig wristMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig intakeMotorConfig = new SparkFlexConfig();
+                
+            public static final SparkFlexConfig IntakeMotorConfig = new SparkFlexConfig();
 
                 static {
 
-                        r_armMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
-                        l_armMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
-                        //r_armMotorConfig.inverted(true);
-                        r_armMotorConfig.follow(12, true);
+                        IntakeMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
 
-                        r_armMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .p(0.6)
-                                        .d(0.5)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(2200)
-                                        .maxAcceleration(2000)
-                                        .allowedClosedLoopError(.1);
-
-                        l_armMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .p(0.6)
-                                        .d(0.5)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(2200)
-                                        .maxAcceleration(2000)
-                                        .allowedClosedLoopError(.1);
+                        IntakeMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                            // Set PID values for position control. We don't need to pass a closed
+                            // loop slot, as it will default to slot 0.
+                            .p(0.4)
+                            .i(0)
+                            .d(0)
+                            .outputRange(-1, 1)
+                            // Set PID values for velocity control in slot 1
+                            .p(0.0001, ClosedLoopSlot.kSlot1)
+                            .i(0, ClosedLoopSlot.kSlot1)
+                            .d(0, ClosedLoopSlot.kSlot1)
+                            .outputRange(-1, 1, ClosedLoopSlot.kSlot1)
+                            .feedForward
+                            // kV is now in Volts, so we multiply by the nominal voltage (12V)
+                            .kV(12.0 / 5767, ClosedLoopSlot.kSlot1);
 
                         // r_armMotorSlowConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
                         // l_armMotorSlowConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
@@ -64,72 +54,37 @@ public final class Configs {
                         //                 .maxAcceleration(1300)
                         //                 .allowedClosedLoopError(.25);
 
-                        l_elevatorMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
-                        r_elevatorMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50).voltageCompensation(12);
-                        r_elevatorMotorConfig.follow(10, true);
-
-                        l_elevatorMotorConfig.closedLoop
-                                        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .p(0.3)
-                                        .d(.6)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(8000)
-                                        .maxAcceleration(8000)
-                                        .allowedClosedLoopError(.5);
-
-                        r_elevatorMotorConfig.closedLoop
-                                        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .p(0.3)
-                                        .d(.6)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(8000)
-                                        .maxAcceleration(8000)
-                                        .allowedClosedLoopError(.5);
-
-                        wristMotorConfig.idleMode(IdleMode.kCoast).smartCurrentLimit(50).voltageCompensation(12);
-
-                        wristMotorConfig.closedLoop
-                                        .feedbackSensor(FeedbackSensor.kAbsoluteEncoder)
-                                        .p(0.01)
-                                        .d(0.01)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(6000)
-                                        .maxAcceleration(8000)
-                                        .allowedClosedLoopError(.009);
-
-                        intakeMotorConfig.inverted(true).idleMode(IdleMode.kBrake).smartCurrentLimit(50);
-
                 }
 
         };
 
-        public static final class FunnelIntakeSubsystem {
-                public static final SparkFlexConfig l_funnelMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig r_funnelMotorConfig = new SparkFlexConfig();
-                public static final SparkFlexConfig funnelWristMotorConfig = new SparkFlexConfig();
+        // public static final class FunnelIntakeSubsystem {
+        //         public static final SparkFlexConfig l_funnelMotorConfig = new SparkFlexConfig();
+        //         public static final SparkFlexConfig r_funnelMotorConfig = new SparkFlexConfig();
+        //         public static final SparkFlexConfig funnelWristMotorConfig = new SparkFlexConfig();
 
-                static {
+        //         static {
 
-                        funnelWristMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
-                        funnelWristMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .p(0.1)
-                                        .outputRange(-1, 1).maxMotion
-                                        .maxVelocity(2000)
-                                        .maxAcceleration(10000)
-                                        .allowedClosedLoopError(.25);
+        //                 funnelWristMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(40).voltageCompensation(12);
+        //                 funnelWristMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        //                                 .p(0.1)
+        //                                 .outputRange(-1, 1).maxMotion
+        //                                 .maxVelocity(2000)
+        //                                 .maxAcceleration(10000)
+        //                                 .allowedClosedLoopError(.25);
 
-                        l_funnelMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
-                        r_funnelMotorConfig.inverted(true).idleMode(IdleMode.kBrake).smartCurrentLimit(50);
+        //                 l_funnelMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
+        //                 r_funnelMotorConfig.inverted(true).idleMode(IdleMode.kBrake).smartCurrentLimit(50);
 
-                }
-        };
+        //         }
+        // };
 
-        public static final class ClimberSubsystem {
-                public static final SparkFlexConfig climberMotorConfig = new SparkFlexConfig();
+        // public static final class ClimberSubsystem {
+        //         public static final SparkFlexConfig climberMotorConfig = new SparkFlexConfig();
 
-                static {
-                        climberMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
+        //         static {
+        //                 climberMotorConfig.idleMode(IdleMode.kBrake).smartCurrentLimit(50);
 
-                }
-        };
+        //         }
+        // };
 }
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,5 +1,46 @@
 package frc.robot.subsystems;
 
-public class Intake {
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import frc.robot.Constants.IntakeConstants;
+import frc.robot.Configs;
+public class Intake extends SubsystemBase {
+   
+    private SparkFlex intakeMotor = new SparkFlex(IntakeConstants.INTAKE_ID, MotorType.kBrushless);
+    private SparkClosedLoopController intakeController = intakeMotor.getClosedLoopController();
+
+    public Intake(){
+        intakeMotor.configure(Configs.IntakeSubsystem.IntakeMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+
+    public void runIntake(){
+        intakeMotor.set(IntakeConstants.INTAKE_SPEED);
+    }
+    public void runOuttake(){
+        intakeMotor.set(IntakeConstants.OUTTAKE_SPEED);
+    }
+    public void stopIntake(){
+        intakeMotor.set(0);
+    }
     
+    public Command runIntakeCommand(){
+        return new RunCommand(() -> runIntake(), this)
+            .finallyDo(interrupted -> stopIntake());
+    }
+
+    public Command runOuttakeCommand(){
+        return new RunCommand(() -> runOuttake(), this)
+            .finallyDo(interrupted -> stopIntake());
+    }
+
+    @Override
+    public void periodic() {
+    }
 }
