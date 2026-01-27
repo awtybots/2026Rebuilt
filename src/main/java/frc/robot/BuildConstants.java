df2e8efa3849880a9c19b88f1be62dc7fc3ab1d3
DavidCui5829 <27cuiz@awty.org>
2026-01-26 22:29:06 -0600

    Create BuildConstants.java

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2026Rebuilt";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 43;
+  public static final String GIT_SHA = "0b1c38546efd638798c628650c71bcb2d4b531f8";
+  public static final String GIT_DATE = "2026-01-26 15:28:38 EST";
+  public static final String GIT_BRANCH = "advantage-scope";
+  public static final String BUILD_DATE = "2026-01-26 19:05:19 EST";
+  public static final long BUILD_UNIX_TIME = 1769472319755L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
