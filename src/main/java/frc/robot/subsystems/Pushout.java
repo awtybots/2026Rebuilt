c570e651af150fb0a36f9c31291bb9ed8a06a1c0
1adityatandon1234-netizen <adityastandon@gmail.com>
2026-02-09 14:02:01 -0600

    adjusted to new motors

--- src/main/java/frc/robot/subsystems/Pushout.java
+++ src/main/java/frc/robot/subsystems/Pushout.java
@@ -27,63 +27,63 @@ public class Pushout extends SubsystemBase {
     // AdvantageKit logging
     private double desiredPercent = 0.0;
 
-    private SparkFlex PushoutLeftMotor = new SparkFlex(PushoutConstants.PUSHOUT_LEFT_ID, MotorType.kBrushless);
-    private SparkClosedLoopController PushoutLeftController = PushoutLeftMotor.getClosedLoopController();
+    // private SparkFlex PushoutLeftMotor = new SparkFlex(PushoutConstants.PUSHOUT_LEFT_ID, MotorType.kBrushless);
+    // private SparkClosedLoopController PushoutLeftController = PushoutLeftMotor.getClosedLoopController();
     private SparkFlex PushoutRightMotor = new SparkFlex(PushoutConstants.PUSHOUT_RIGHT_ID, MotorType.kBrushless);
     private SparkClosedLoopController PushoutRightController = PushoutRightMotor.getClosedLoopController();
 
-    private final RelativeEncoder pushoutLeftEncoder = PushoutLeftMotor.getEncoder();
+    // private final RelativeEncoder pushoutLeftEncoder = PushoutLeftMotor.getEncoder();
     private final RelativeEncoder pushoutRightEncoder = PushoutRightMotor.getEncoder();
 
     private double PushoutRightExtended = PushoutConstants.PUSHOUT_EXTENDED_POS;
-    private double PushoutLeftExtended = PushoutConstants.PUSHOUT_EXTENDED_POS;
+    // private double PushoutLeftExtended = PushoutConstants.PUSHOUT_EXTENDED_POS;
     private double PushoutRightRetracted = PushoutConstants.PUSHOUT_RETRACTED_POS;
-    private double PushoutLeftRetracted = PushoutConstants.PUSHOUT_RETRACTED_POS;
+    // private double PushoutLeftRetracted = PushoutConstants.PUSHOUT_RETRACTED_POS;
     private double PushoutRightExtendedAgitate = PushoutConstants.PUSHOUT_EXTENDED_AGITATE_POS;
-    private double PushoutLeftExtendedAgitate = PushoutConstants.PUSHOUT_EXTENDED_AGITATE_POS;
+    // private double PushoutLeftExtendedAgitate = PushoutConstants.PUSHOUT_EXTENDED_AGITATE_POS;
     private double PushoutRightRetractedAgitate = PushoutConstants.PUSHOUT_RETRACTED_AGITATE_POS;
-    private double PushoutLeftRetractedAgitate = PushoutConstants.PUSHOUT_RETRACTED_AGITATE_POS;
+    // private double PushoutLeftRetractedAgitate = PushoutConstants.PUSHOUT_RETRACTED_AGITATE_POS;
 
     public Pushout() {
-        PushoutLeftMotor.configure(Configs.PushoutSubsystem.PushoutLeftMotorConfig, ResetMode.kResetSafeParameters,
-                PersistMode.kPersistParameters);
+        // PushoutLeftMotor.configure(Configs.PushoutSubsystem.PushoutLeftMotorConfig, ResetMode.kResetSafeParameters,
+        //         PersistMode.kPersistParameters);
         PushoutRightMotor.configure(Configs.PushoutSubsystem.PushoutRightMotorConfig, ResetMode.kResetSafeParameters,
                 PersistMode.kPersistParameters);
     }
 
     public void PushIntake() {
         // finds the setpoint compared to encoder positions so the pushout moves by the offsets from where it is
-        double leftNow = pushoutLeftEncoder.getPosition();
+        // double leftNow = pushoutLeftEncoder.getPosition();
         double rightNow = pushoutRightEncoder.getPosition();
-        PushoutLeftController.setSetpoint(leftNow + PushoutLeftExtended, ControlType.kMAXMotionPositionControl);
+        // PushoutLeftController.setSetpoint(leftNow + PushoutLeftExtended, ControlType.kMAXMotionPositionControl);
         PushoutRightController.setSetpoint(rightNow + PushoutRightExtended, ControlType.kMAXMotionPositionControl);
 
     }
     public void RetractIntake() {
-        double leftNow = pushoutLeftEncoder.getPosition();
+        // double leftNow = pushoutLeftEncoder.getPosition();
         double rightNow = pushoutRightEncoder.getPosition();
-        PushoutLeftController.setSetpoint(leftNow + PushoutLeftRetracted, ControlType.kMAXMotionPositionControl);
+        // PushoutLeftController.setSetpoint(leftNow + PushoutLeftRetracted, ControlType.kMAXMotionPositionControl);
         PushoutRightController.setSetpoint(rightNow + PushoutRightRetracted, ControlType.kMAXMotionPositionControl);
     }
 
     public void SmallPush() 
     {
-        double leftNow = pushoutLeftEncoder.getPosition();
+        // double leftNow = pushoutLeftEncoder.getPosition();
         double rightNow = pushoutRightEncoder.getPosition();
-        PushoutLeftController.setSetpoint(leftNow + PushoutLeftExtendedAgitate, ControlType.kMAXMotionPositionControl);
+        // PushoutLeftController.setSetpoint(leftNow + PushoutLeftExtendedAgitate, ControlType.kMAXMotionPositionControl);
         PushoutRightController.setSetpoint(rightNow + PushoutRightExtendedAgitate, ControlType.kMAXMotionPositionControl);
     }
 
     public void SmallRetract() 
     {
-        double leftNow = pushoutLeftEncoder.getPosition();
+        // double leftNow = pushoutLeftEncoder.getPosition();
         double rightNow = pushoutRightEncoder.getPosition();
-        PushoutLeftController.setSetpoint(leftNow + PushoutLeftRetractedAgitate, ControlType.kMAXMotionPositionControl);
+        // PushoutLeftController.setSetpoint(leftNow + PushoutLeftRetractedAgitate, ControlType.kMAXMotionPositionControl);
         PushoutRightController.setSetpoint(rightNow + PushoutRightRetractedAgitate, ControlType.kMAXMotionPositionControl);
     }
 
     public void StopPushing() {
-        PushoutLeftController.setSetpoint(0, ControlType.kMAXMotionPositionControl);
+        // PushoutLeftController.setSetpoint(0, ControlType.kMAXMotionPositionControl);
         PushoutRightController.setSetpoint(0, ControlType.kMAXMotionPositionControl);
     }
 
@@ -118,6 +118,6 @@ public class Pushout extends SubsystemBase {
         // Commanded intake motor percent output.
         Logger.recordOutput("Pushout/DesiredPercent", desiredPercent);
         // Applied voltage to intake motor.
-        Logger.recordOutput("Pushout/AppliedVolts", PushoutLeftMotor.getAppliedOutput() * PushoutLeftMotor.getBusVoltage());
+        // Logger.recordOutput("Pushout/AppliedVolts", PushoutLeftMotor.getAppliedOutput() * PushoutLeftMotor.getBusVoltage());
     }
 }
