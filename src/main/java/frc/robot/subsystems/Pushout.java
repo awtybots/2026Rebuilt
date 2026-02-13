d2a99ed826f3ea3d5ca07ed2adfbd77eb2a381ff
Ishaan <guptai@awty.org>
2026-02-12 22:46:53 -0600

    Duty Cycle for Pushout

--- src/main/java/frc/robot/subsystems/Pushout.java
+++ src/main/java/frc/robot/subsystems/Pushout.java
@@ -83,6 +83,16 @@ public class Pushout extends SubsystemBase {
         // PushoutRightController.setSetpoint(PushoutRightRetractedAgitate, ControlType.kMAXMotionPositionControl);
     }
 
+    public void PushoutDutyCycle()
+    {
+        PushoutLeftMotor.set(0.3);
+    }
+
+    public void StopPushout()
+    {
+        PushoutLeftMotor.set(0);
+    }
+
     // public void StopPushing() {
     //     // PushoutLeftController.setSetpoint(0, ControlType.kMAXMotionPositionControl);
     //     PushoutRightController.setSetpoint(0, ControlType.kMAXMotionPositionControl);
@@ -99,8 +109,8 @@ public class Pushout extends SubsystemBase {
 
 
     public Command PushCommand() {
-        return new RunCommand(() -> PushIntake(), this)
-                .finallyDo(interrupted -> PushIntake());
+        return new RunCommand(() -> PushoutDutyCycle(), this)
+                .finallyDo(interrupted -> StopPushout());
     }
 
     public Command RetractCommand() {
