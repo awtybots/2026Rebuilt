dc849001a65d81f91b94d80ed3520d233d0ece6a
DavidCui5829 <27cuiz@awty.org>
2026-02-17 16:04:05 -0600

    lut

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -107,10 +107,14 @@ public final class Configs
                                 kickerLeftMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                                 // Set PID values for position control. We don't need to pass a closed
                                 // loop slot, as it will default to slot 0.
-                                .p(0.0002355)
+                                .p(0.0001355)
                                 .i(0)
                                 .d(0)
-                                .outputRange(-1, 1);
+                                .outputRange(-1, 1)
+                                 .feedForward
+                                .kS(0.10)
+                                .kV(0.00177)
+                                .kA(0.00017);
                                 kickerLeftMotorConfig.closedLoop
                                 .maxMotion.maxAcceleration(10000);
 
@@ -118,10 +122,15 @@ public final class Configs
                                 kickerRightMotorConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                                 // Set PID values for position control. We don't need to pass a closed
                                 // loop slot, as it will default to slot 0.
-                                .p(0.0002355)
+                                .p(0.0001355)
                                 .i(0)
                                 .d(0)
-                                .outputRange(-1, 1);
+                                .outputRange(-1, 1)
+                                 .feedForward
+                                .kS(0.10)
+                                .kV(0.00177)
+                                .kA(0.00017);
+
                                 kickerRightMotorConfig.closedLoop
                                 .maxMotion.maxAcceleration(10000);
                         }
@@ -228,10 +237,16 @@ public final class Configs
                         TwindexerRightControllerConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                             // Set PID values for  position control. We don't need to pass a closed
                             // loop slot, as it will default to slot 0.
-                            .p(0.001)
+                            .p(0.0001)
                             .i(0)
                             .d(0)
-                            .outputRange(-1, 1);
+                            .outputRange(-1, 1)
+                             .feedForward
+                                .kS(0.10)
+                                .kV(0.00177)
+                                .kA(0.00017);
+                        TwindexerRightControllerConfig.closedLoop
+                                .maxMotion.maxAcceleration(10000);
                             // Set PID values for velocity control in slot 1
                         //     .p(0.0001, ClosedLoopSlot.kSlot1)
                         //     .i(0, ClosedLoopSlot.kSlot1)
@@ -245,10 +260,16 @@ public final class Configs
                             TwindexerLeftControllerConfig.closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
                             // Set PID values for position control. We don't need to pass a closed
                             // loop slot, as it will default to slot 0.
-                            .p(0.001)
+                            .p(0.0001)
                             .i(0)
                             .d(0)
-                            .outputRange(-1, 1);
+                            .outputRange(-1, 1)
+                             .feedForward
+                                .kS(0.10)
+                                .kV(0.00177)
+                                .kA(0.00017);
+                        TwindexerLeftControllerConfig.closedLoop
+                                .maxMotion.maxAcceleration(10000);
                             // Set PID values for velocity control in slot 1
                         //     .p(0.0001, ClosedLoopSlot.kSlot1)
                         //     .i(0, ClosedLoopSlot.kSlot1)
--- src/main/java/frc/robot/commands/VariableShoot.java
+++ src/main/java/frc/robot/commands/VariableShoot.java
@@ -62,9 +62,9 @@ public class VariableShoot extends Command
     for (var entry : List.of(
       // Pair.of(Meters.of(1), RPM.of((1000))),
                             //  Pair.of(Meters.of(2), RPM.of(1622.8225)),
-                             Pair.of(Meters.of(3), RPM.of(1900))
+                             Pair.of(Meters.of(3), RPM.of(1900)),
                             //  Pair.of(Meters.of(3.373), RPM.of(1897.454)),
-                            //  Pair.of(Meters.of(4), RPM.of(2022.2865)),
+                              Pair.of(Meters.of(4), RPM.of(2200))
                             //  Pair.of(Meters.of(5.2048), RPM.of(2296.918))
                             )
     )
